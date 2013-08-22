f60229f85f9d44679a6cc5a1ac960297f4723bab
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2013-08-22 23:03:40 +0200

    Actions!

--- ts-core/ts-actions.php
+++ ts-core/ts-actions.php
@@ -9,5 +9,25 @@
 // Exit if accessed directly
 if (!defined('ABSPATH')) exit;
 
-add_action('plugins_loaded', 'ts_fields', 10);
-add_action('plugins_loaded', 'ts_admin', 11);
+add_action('plugins_loaded',    'ts_loaded', 10);
+add_action('after_setup_theme'  'ts_after_setup_theme', 10);
+
+if (is_admin()) {
+  add_action('ts_loaded', 'ts_admin');
+}
+
+/**
+ * Plugins loaded action.
+ */
+
+function ts_loaded () {
+  do_action('ts_loaded');
+}
+
+/**
+ * After setup theme action.
+ */
+
+function ts_after_setup_theme () {
+  do_action('ts_after_setup_theme');
+}
